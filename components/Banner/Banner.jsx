76e6229a4856c9e3c6594a5e3e789997911d5937
EloraBarua <elorabarua62@gmail.com>
2024-03-08 09:52:04 +0600

    banner section and responsive done

--- components/Banner/Banner.jsx
+++ components/Banner/Banner.jsx
@@ -8,16 +8,64 @@ const Banner = () => {
     return (
       <div className={styles.banner_display}>
         <div className={styles.column1}>
-            
+          <Image
+            src={banner_image.barath_long}
+            alt=""
+            fill
+            className={styles.image_design1}
+          ></Image>
         </div>
         <div className={styles.column2}>
-            
+          <div className={styles.colage_design}>
+            <Image
+              src={banner_image.sitar}
+              alt=""
+              fill
+              className={styles.image_design2}
+            ></Image>
+          </div>
+          <div className={styles.colage_design}>
+            <Image
+              src={banner_image.paint}
+              alt=""
+              fill
+              className={styles.image_design2}
+            ></Image>
+          </div>
         </div>
         <div className={styles.column3}>
-            
+          <Image
+            src={banner_image.kathak}
+            alt=""
+            fill
+            className={styles.image_design1}
+          ></Image>
         </div>
         <div className={styles.column4}>
-            
+          <div className={styles.colage_design}>
+            <Image
+              src={banner_image.tabla}
+              alt=""
+              fill
+              className={styles.image_design2}
+            ></Image>
+          </div>
+          <div className={styles.colage_design}>
+            <Image
+              src={banner_image.harmon}
+              alt=""
+              fill
+              className={styles.image_design2}
+            ></Image>
+          </div>
+        </div>
+        <div className={styles.column5}>
+          <Image
+            src={banner_image.barath_short}
+            alt=""
+            fill
+            className={styles.image_design1}
+          ></Image>
         </div>
       </div>
     );
