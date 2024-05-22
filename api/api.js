017b585a74a1bc3ca323c3d214e16bbe14cc3c4d
EloraBarua <elorabarua62@gmail.com>
2024-05-22 20:57:06 +0600

    News and marquee design has been changed

--- api/api.js
+++ api/api.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-  // baseURL: "https://api.msh6545.knowme.sbs/api/v1",
-  baseURL: "http://localhost:5000/api/v1",
+  baseURL: "https://api.msh6545.knowme.sbs/api/v1",
+  // baseURL: "http://localhost:5000/api/v1",
 });
 
 export default api;
