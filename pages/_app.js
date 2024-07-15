fcf2132d4c3eb92106da9ad0fb0c52e4b6ee6c50
EloraBarua <elorabarua62@gmail.com>
2024-07-16 00:29:40 +0600

    admit card design, certificate design testing

--- pages/_app.js
+++ pages/_app.js
@@ -1,34 +1,52 @@
 import store from "@component/app/store";
 import Layout from "@component/components/shared/Layout";
 import "@component/styles/globals.scss";
+import Head from "next/head";
 import { usePathname } from "next/navigation";
-import { useEffect, useLayoutEffect, useState } from "react";
 import { Toaster } from "react-hot-toast";
 import { Provider } from "react-redux";
 
+
 export default function App({ Component, pageProps }) {
-  const [role, setRole] = useState("");
   const pathname = usePathname();
 
   return (
-    <Provider store={store}>
-      {(pathname === "/admin" || pathname === "/admin/login") ? (
-        <Component {...pageProps} />
-      ) : (
-        <Layout>
-          <Component {...pageProps} />{" "}
-        </Layout>
-      )}
+    <>
+      <Head>
+        <title>Dhurva Parishad</title>
+        <meta
+          name="description"
+          content="This is a website of Dhurva Parishad for exam and result management"
+        />
+        <meta name="keywords" content="Dhurva, Parishad, Bangladesh" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta property="og:title" content="Dhurva Parishad Bangladesh" />
+        <meta
+          property="og:description"
+          content="Dhurva Parishad Exam & Result management"
+        />
+        <meta property="og:image" content="/public/logo.jpeg" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Provider store={store}>
+        {pathname === "/admin" || pathname === "/admin/login" ? (
+          <Component {...pageProps} />
+        ) : (
+          <Layout>
+            <Component {...pageProps} />{" "}
+          </Layout>
+        )}
 
-      <Toaster
-        toastOptions={{
-          position: "top-right",
-          style: {
-            background: "#283046",
-            color: "white",
-          },
-        }}
-      />
-    </Provider>
+        <Toaster
+          toastOptions={{
+            position: "top-right",
+            style: {
+              background: "#283046",
+              color: "white",
+            },
+          }}
+        />
+      </Provider>
+    </>
   );
 }
--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,9 @@
 import "../styles/globals.scss";
 
-export const metadata = {
-  title: "Dhurva Parishad",
-  description:
-    "This is a website of Dhurva Parishad for exam and result management",
-  metadataBase: new URL("https://dhruva-parisad-clientside.vercel.app/user"),
-};
-
-export default function RootLayout() {
+export default function Home() {
   return (
-    <>
-        This is client side dhruba parishad     
-    </>
+      <>
+        Dhruva Parishad
+      </>
   );
 }
